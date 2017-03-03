0f0d55cf0fcec1dd61bd7e7be6a087446b7aba9b
Flug <flugv1@gmail.com>
2017-03-03 17:25:04 +0100

    fix cs

--- app/ConsoleKernel.php
+++ app/ConsoleKernel.php
@@ -1,15 +1,16 @@
 <?php
 
-class ConsoleKernel {
-
-   public static function commands(){
-
-      return [
-         new Clooder\Command\WebsocketCommand()
-      ];
-   }
-
-   public static function getRootKernel(){
-      return __DIR__;
-   }
+class ConsoleKernel
+{
+    public static function commands()
+    {
+        return [
+            new Clooder\Command\WebsocketCommand(),
+        ];
+    }
+    
+    public static function getRootKernel()
+    {
+        return __DIR__;
+    }
 }
